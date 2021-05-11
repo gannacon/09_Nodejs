dda639d09edb92d4272059b3250c938aea0bb097
gannacon <cwgannaway@gmail.com>
2021-05-10 21:41:22 -0500

    adding more boilerplate and function to generate questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,55 @@
 // TODO: Include packages needed for this application
+// ADDED FS
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your user name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your location?",
+    name: "location",
+  },
+  {
+    type: "input",
+    message: "Provide a brief bio of yourself:",
+    name: "bio",
+  },
+  {
+    type: "input",
+    message: "Provide your LinkedIn URL:",
+    name: "linkedIn",
+  },
+  {
+    type: "input",
+    message: "Provide your gitHub URL:",
+    name: "gitHub",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const README = `test`;
+  fs.writeFile("index.html", README, (err) =>
+    err ? console.error(err) : console.log("Commit logged!")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt(questions)
+
+    .then((response) => {
+      console.log(response);
+      writeToFile(response);
+    });
+}
 
 // Function call to initialize app
 init();
